9f623e67d548b7f9d7d679bede3529cdc9e57f50
caesium <eka.caesium721@gmail.com>
2024-07-02 03:49:53 +0600

    login button not showing

--- QuickBites/src/components/LoginPopup/LoginPopup.jsx
+++ QuickBites/src/components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = (setShowLogin) => {
+
+  const [currState, setCurrState] = useState("Sign Up")
+
   return (
     <div className='login-popup'>
-      login
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" /> 
+        </div>
+        <div className="login-popup-inputs">
+          {currState==="Login"?<></>:<input type="text" placeholder='Your Name' required />}
+          
+          <input type="email" placeholder='Your email ' required/>
+          <input type="password" placeholder='Password ' required/>
+        </div>
+        <button>
+          {currState==="Sign Up"?"Create account":"Login"}
+        </button>
+        </form>
     </div>
   )
 }
+  
+
 
 export default LoginPopup
