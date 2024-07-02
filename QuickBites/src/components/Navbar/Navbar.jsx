7d745517445f60c81083b60b7082f14b590ff8ab
caesium <eka.caesium721@gmail.com>
2024-07-02 16:11:58 +0600

    login page done, cart css done ,orderpage css done,checkout done

--- QuickBites/src/components/Navbar/Navbar.jsx
+++ QuickBites/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 
 
    const [menu,setMenu] = useState("menu");
+   const {getTotalCartAmount}=useContext(StoreContext);
 
 
   return (
@@ -22,7 +24,7 @@ const Navbar = ({setShowLogin}) => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
             <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-            <div className="dot"></div>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
