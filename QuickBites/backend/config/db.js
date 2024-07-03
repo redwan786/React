8b4915d03167277fea87a4d67e9ab603c4e25d77
MD.JAKARIA <jakariamahmud76@gmail.com>
2024-07-03 13:10:51 +0600

    fresh

--- QuickBites/backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:138141147@cluster0.qnyyty4.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-}
