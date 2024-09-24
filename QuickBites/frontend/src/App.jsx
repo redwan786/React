26896cd04f1eb320aaf4cf33c02e83b3d57c4194
MD.JAKARIA <jakariamahmud76@gmail.com>
2024-09-25 04:02:03 +0600

    navbar change

--- QuickBites/frontend/src/App.jsx
+++ QuickBites/frontend/src/App.jsx
@@ -1,6 +1,5 @@
-
 import Navbar from "./components/Navbar/Navbar"
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from "./pages/Cart/Cart"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
@@ -8,29 +7,29 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Footer from './components/Footer/Footer';
 import { useState } from "react"
 import LoginPopup from "./components/LoginPopup/LoginPopup"
+import Verify from './pages/Verify/verify';
+import MyOrders from "./pages/MyOrders/MyOrders"
 
 const App = () => {
-  
-  const[showLogin,setShowLogin]=useState(false)
 
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Cart' element={<Cart/>} />
-        <Route path='/order' element ={<PlaceOrder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify />} />
+          <Route path='/myorders' element={<MyOrders />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-    
   )
 }
 
 export default App
-
-
